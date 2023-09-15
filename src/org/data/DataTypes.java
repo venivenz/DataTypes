9371fca9ca4827b802c295c141782bac984e9bae
venivenz <stkrishnaveni99@gmail.com>
2023-09-15 02:49:09 -0700

    first commit

---
+++ src/org/data/DataTypes.java
@@ -0,0 +1,18 @@
+package org.data;
+
+public class DataTypes {
+	public static void main(String[] args){
+		byte age = 24;
+		System.out.println(age);
+		System.out.println("My age is"+age);
+		
+		short atmpin=5555;
+		System.out.println(atmpin);
+		System.out.println("My atmpin is"+atmpin);
+		
+		int pincode=627428;
+		System.out.println(pincode);
+		System.out.println("My pincode is"+pincode);
+	}
+
+}
